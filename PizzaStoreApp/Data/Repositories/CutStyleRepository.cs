9bb39816cec42d0cf44a77f09053013fc5d608cf
Kevin Hudson <khudson@midfinance.com>
2022-02-06 15:11:15 -0500

    Continued work with the PizzaBuilder. Created Custom Validation css

---
+++ PizzaStoreApp/Data/Repositories/CutStyleRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace PizzaStoreApp.Data.Repositories
+{
+    public class CutStyleRepository : ICutStyleRepository
+    {
+        private readonly IDbContextFactory<DataContext> _context;
+        public CutStyleRepository(IDbContextFactory<DataContext> context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<CutStyle>> GetAllCutStylesAsync()
+        {
+            using (var ctx = _context.CreateDbContext())
+            {
+                var result = await ctx.CutStyles.ToListAsync();
+                return result;
+
+            }
+
+        }
+
+        public async Task<CutStyle> GetCutStyleByIdAsync(int id)
+        {
+            using (var ctx = _context.CreateDbContext())
+            {
+
+                var result = await ctx.CutStyles.FirstOrDefaultAsync(x => x.Id == id);
+                if (result == null)
+                {
+                    return null;
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ PizzaStoreApp/Interfaces/ICutStyleRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PizzaStoreApp.Interfaces
+{
+    public interface ICutStyleRepository
+    {
+        Task<List<CutStyle>> GetAllCutStylesAsync();
+        Task<CutStyle> GetCutStyleByIdAsync(int id);
+    }
+}
